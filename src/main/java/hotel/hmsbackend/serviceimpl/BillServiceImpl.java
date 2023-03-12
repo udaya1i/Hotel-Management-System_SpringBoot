642374c28aa1c3b8e31792bb23edd4af97911dc1
udaya <ping.udaya@gmail.com>
2023-03-12 15:04:52 +0545

    Generating the Bill and downloading on device

--- src/main/java/hotel/hmsbackend/serviceimpl/BillServiceImpl.java
+++ src/main/java/hotel/hmsbackend/serviceimpl/BillServiceImpl.java
@@ -1,4 +1,5 @@
 package hotel.hmsbackend.serviceimpl;
+
 import com.google.gson.JsonArray;
 import com.google.gson.annotations.JsonAdapter;
 import com.itextpdf.text.*;
@@ -11,8 +12,10 @@ import hotel.hmsbackend.jwt.JwtFilter;
 import hotel.hmsbackend.pojo.Bill;
 import hotel.hmsbackend.service.BillService;
 import hotel.hmsbackend.utils.HMSUtilits;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.asm.Advice;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ReactiveHttpInputMessage;
@@ -30,51 +33,71 @@ public class BillServiceImpl implements BillService {
     JwtFilter jwtFilter;
     @Autowired
     BillDao billDao;
+
     @Override
     public ResponseEntity<String> generateReport(Map<String, Object> requestMap) {
         log.info("generateReport");
-        try{
+        try {
             String fileName;
-            if (validateRequestMap(requestMap)){
-                if (requestMap.containsKey("isGenerate") && !(Boolean) requestMap.get("isGenerate")){
+            if (validateRequestMap(requestMap)) {
+                if (requestMap.containsKey("isGenerate") && !(Boolean) requestMap.get("isGenerate")) {
                     fileName = (String) requestMap.get("uuid");
-                }else {
+                } else {
                     fileName = HMSUtilits.getUUID();
                     requestMap.put("uuid", fileName);
                     insertBill(requestMap);
                 }
-                String data = "Name: "+requestMap.get("name")+"\n"+"Contact Number: "+requestMap.get("contactNumber")+
-                        "\n"+"Email: "+requestMap.get("email")+"\n"+"Payment Method: "+requestMap.get("paymentMethod");
+                String data = "Name: " + requestMap.get("name") + "\n" + "Contact Number: " + requestMap.get("contactNumber") +
+                        "\n" + "Email: " + requestMap.get("email") + "\n" + "Payment Method: " + requestMap.get("paymentMethod");
 
                 Document document = new Document();
-                PdfWriter.getInstance(document, new FileOutputStream(HMSConstant.stored_location+"\\"+fileName+".pdf"));
+                PdfWriter.getInstance(document, new FileOutputStream(HMSConstant.stored_location + "\\" + fileName + ".pdf"));
 
                 //doc.....
                 document.open();
                 setRectangleInPdf(document);
-                Paragraph paragr = new Paragraph("Bill",getFont("Header"));
+                Paragraph paragr = new Paragraph("Bill", getFont("Header"));
                 paragr.setAlignment(Element.ALIGN_CENTER);
                 document.add(paragr);
-                Paragraph paragraph = new Paragraph(data+"\n \n", getFont("Data"));
+                Paragraph paragraph = new Paragraph(data + "\n \n", getFont("Data"));
                 document.add(paragraph);
                 PdfPTable table = new PdfPTable(6);
                 table.setWidthPercentage(100);
                 addTableHeader(table);
 
-                JsonArray jsonArray =
+                JSONArray jsonArray = HMSUtilits.getJsonArrayFromString((String) requestMap.get("productDetails"));
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    addRows(table, HMSUtilits.getMapFromJson(jsonArray.getString(i)));
+                }
+                document.add(table);
+
+                Paragraph footer = new Paragraph("Total : " + requestMap.get("total") + "\n" + "Thank Your for Visiting Us, See you Soon", getFont("Data"));
+                document.add(footer);
+                document.close();
+
+                return new ResponseEntity<>("{\"uuid\":\"" + fileName + "\"}", HttpStatus.OK);
 
             }
             return HMSUtilits.getResponseEntity("Data Not Found", HttpStatus.BAD_REQUEST);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return HMSUtilits.getResponseEntity(HMSConstant.something_went_wrong, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    private void addRows(PdfPTable table, Map<String, Object> mapFromJson) {
+        log.info("addrows....");
+        table.addCell((String) mapFromJson.get("name"));
+        table.addCell((String) mapFromJson.get("category"));
+        table.addCell((String) mapFromJson.get("qty"));
+        table.addCell(Double.toString((Double) mapFromJson.get("price")));
+        table.addCell(Double.toString((Double) mapFromJson.get("total")));
+    }
+
     private void addTableHeader(PdfPTable table) {
-    log.info("TableHeader ................");
+        log.info("TableHeader ................");
         Stream.of("Name", "Category", "QTY", "Price", "Total", "Discount")
-                .forEach(columnHeader->{
+                .forEach(columnHeader -> {
                     PdfPCell header = new PdfPCell();
                     header.setBackgroundColor(BaseColor.LIGHT_GRAY);
                     header.setBorderWidth(2);
@@ -89,7 +112,7 @@ public class BillServiceImpl implements BillService {
 
     private Font getFont(String header) {
         log.info("getfont-------");
-        switch (header){
+        switch (header) {
             case "Header":
                 Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLDOBLIQUE, 20, BaseColor.RED);
                 headerFont.setStyle(Font.BOLD);
@@ -101,30 +124,30 @@ public class BillServiceImpl implements BillService {
             default:
                 return new Font();
         }
-
     }
 
     private void setRectangleInPdf(Document document) throws DocumentException {
-    log.info("inside documentation");
-        Rectangle rectangle = new Rectangle(500, 800, 20 , 20);
+        log.info("inside documentation");
+        Rectangle rectangle = new Rectangle(500, 800, 20, 20);
         rectangle.enableBorderSide(1);
         rectangle.enableBorderSide(2);
         rectangle.enableBorderSide(4);
         rectangle.enableBorderSide(8);
         rectangle.setBackgroundColor(BaseColor.BLUE);
+        rectangle.setBorderColor(BaseColor.BLACK);
         rectangle.setBorderWidth(1);
         document.add(rectangle);
     }
 
-
     private boolean validateRequestMap(Map<String, Object> requestMap) {
-        return  requestMap.containsKey("name") &&
+        return requestMap.containsKey("name") &&
                 requestMap.containsKey("contactNumber") &&
                 requestMap.containsKey("email") &&
                 requestMap.containsKey("paymentMethod") &&
                 requestMap.containsKey("productDetail") &&
                 requestMap.containsKey("total");
     }
+
     private void insertBill(Map<String, Object> requestMap) {
         try {
             Bill bill = new Bill();
@@ -132,12 +155,12 @@ public class BillServiceImpl implements BillService {
             bill.setName((String) requestMap.get("name"));
             bill.setEmail((String) requestMap.get("email"));
             bill.setContactNumber((String) requestMap.get("contactNumber"));
-            bill.setPaymentMethod((String)requestMap.get("paymentMethod"));
+            bill.setPaymentMethod((String) requestMap.get("paymentMethod"));
             bill.setTotal(Integer.parseInt((String) requestMap.get("total")));
             bill.setProductDetail((String) requestMap.get("setProductDetail"));
             bill.setCreatedBy(jwtFilter.getCurrentUser());
             billDao.save(bill);
-         }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
--- src/main/java/hotel/hmsbackend/utils/HMSUtilits.java
+++ src/main/java/hotel/hmsbackend/utils/HMSUtilits.java
@@ -1,12 +1,14 @@
 package hotel.hmsbackend.utils;
-import com.fasterxml.jackson.core.io.JsonEOFException;
-import com.google.gson.JsonArray;
+import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import java.util.Date;
-
+import java.util.HashMap;
+import java.util.Map;
 public class HMSUtilits {
     public HMSUtilits() {
     }
@@ -18,10 +20,16 @@ public class HMSUtilits {
         long time = date.getTime();
         return "Bill -"+time;
     }
-
-    public static JsonArray getJsonArrayFromString(String data) throws JSONException {
-        JsonArray jsonArray = new JsonArray(data);
-
-
+    public static JSONArray getJsonArrayFromString(String data) throws JSONException {
+        JSONArray jsonArray =  new JSONArray(data);
+        return jsonArray;
     }
-}
+    public static Map<String, Object> getMapFromJson(String data){
+        if (!Strings.isNullOrEmpty(data))
+            return new Gson().fromJson(data, new TypeToken<Map<String, Object>>(){
+
+            }.getType());
+            return  new HashMap<>();
+        }
+    }
+
